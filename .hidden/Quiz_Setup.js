ca2b385032c20ff7aa8b3ad3caaf6cea65b12988
YasminaaAyman <49916285+YasminaaAyman@users.noreply.github.com>
2025-03-26 23:24:33 +0200

    Update Quiz_Setup.js

--- .hidden/Quiz_Setup.js
+++ .hidden/Quiz_Setup.js
@@ -1,3 +1,19 @@
+// Full Screen Mode
+function go_fullscreen() {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+    }
+}
+
+go_fullscreen()
+
+
 function disableCopyPaste() {
     // Prevent right-click context menu
     document.addEventListener("contextmenu", function(event) {
@@ -15,43 +31,7 @@ function disableCopyPaste() {
         e.preventDefault();
     });
 
-    
-// Apply when notebook is fully loaded
-events.on("notebook_loaded", disableCopyPaste);
-if (Jupyter.notebook._fully_loaded) {
-    disableCopyPaste();
-}
-
-
-// Full Screen Mode
-function go_fullscreen() {
-    if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-    } else if (document.documentElement.mozRequestFullScreen) {
-        document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullscreen) {
-        document.documentElement.webkitRequestFullscreen();
-    } else if (document.documentElement.msRequestFullscreen) {
-        document.documentElement.msRequestFullscreen();
-    }
-}
-
-go_fullscreen()
-
-
-// Disable Copy/Paste (Keyboard Shortcut)
-// document.addEventListener("keydown", function(e) {
-//     if ((e.ctrlKey || e.metaKey) && (e.key === "c" || e.key === "v")) {
-//         e.clipboardData.setData("text/plain", "Copy and Paste are disabled :)");
-//         e.preventDefault();
-//     }
-// });
-
-
-// Disable Copy/paste (Right-Click Menu)
-// document.addEventListener("contextmenu", function (event) {
-//     event.preventDefault();
-// });
+disableCopyPaste()
 
 
 // Wait until Jupyter Notebook is fully loaded and delete some buttons in toolbar
