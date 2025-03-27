526c234f91089a6ef501c8b1940f58836be89ac8
YasminaaAyman <49916285+YasminaaAyman@users.noreply.github.com>
2025-03-27 17:46:28 +0200

    Update Quiz_Setup.js

--- .hidden/Quiz_Setup.js
+++ .hidden/Quiz_Setup.js
@@ -13,6 +13,20 @@ function go_fullscreen() {
 
 go_fullscreen()
 
+// -------------------------------------------------------------------------------------------------------
+
+// Wait until Jupyter Notebook is fully loaded and delete some buttons in toolbar
+setTimeout(function () {
+    document.querySelectorAll('.jp-ToolbarButtonComponent').forEach(el => {
+        if (el.title === "Visit Binder repository" || el.title === "Link to this Binder" || 
+            el.title === "Download visible" || el.title === "Save to browser storage" ||
+            el.title === "Restore from browser storage" || el.title === "Notebook") {
+            el.style.display = 'none';
+        }
+    });
+}, 1000);
+
+// -------------------------------------------------------------------------------------------------------
 
 // Disable copy-paste for the entire notebook
 const disableCopyPaste = (e) => {
@@ -44,15 +58,3 @@ document.addEventListener('keydown', (e) => {
 document.addEventListener("contextmenu", function (event) {
     event.preventDefault();
 });
-
-
-// Wait until Jupyter Notebook is fully loaded and delete some buttons in toolbar
-setTimeout(function () {
-    document.querySelectorAll('.jp-ToolbarButtonComponent').forEach(el => {
-        if (el.title === "Visit Binder repository" || el.title === "Link to this Binder" || 
-            el.title === "Download visible" || el.title === "Save to browser storage" ||
-            el.title === "Restore from browser storage" || el.title === "Notebook") {
-            el.style.display = 'none';
-        }
-    });
-}, 1000);
